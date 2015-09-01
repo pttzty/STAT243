a4d9d9b7c92fe5145e602793c9aec14c396e5d3b
pttzty <tyzhang@berkeley.edu>
2015-08-31 18:41:47 -0700

    This is my information.

--- info.R
+++ info.R
@@ -1,4 +1,4 @@
-name = Tianyi Zhang
-SID = 26955248
-email = tyzhang@berkeley.edu
-github_name = pttzty
+name = "Tianyi Zhang"
+SID = "26955248"
+email = "tyzhang@berkeley.edu"
+github_name = "pttzty"
