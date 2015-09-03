a5cee2948579bfd7e1f83dfdb729a7a1a065f329
pttzty <tyzhang@berkeley.edu>
2015-09-02 17:03:31 -0700

    change spe2

--- ps1/ps1bash.sh
+++ ps1/ps1bash.sh
@@ -1,4 +1,11 @@
+##Problem1
+##Part A
 ##Download the compressed data file to local
 wget --output-document data.zip "http://data.un.org/Handlers/DownloadHandler.ashx?DataFilter=itemCode:526&DataMartId=FAO&Format=csv&c=2,3,4,5,6,7&s=countryName:asc,elementCode:asc,year:desc"
-##uncompress the file
+##uncompress the file 
 unzip data.zip
+git mv -k UNdata_Export_20150902_062742765.csv data.csv
+##Countries into one file, world to the other.
+grep "+" data.csv > regions.csv
+grep -v "+" data.csv > countries.csv
+
