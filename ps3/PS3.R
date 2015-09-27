a48e27f4e220cee8605453011ecb7c5ab54ce6e7
pttzty <tyzhang@berkeley.edu>
2015-09-26 20:20:27 -0700

    regular change

--- ps3/PS3.R
+++ ps3/PS3.R
@@ -29,22 +29,30 @@ first_html<-str_replace_all(first_html,". title.*","")
 Dateinfo<-selectyear[grep("First",selectyear)]
 Dateinfo<-as.data.frame.Date(str_extract(Dateinfo,"(September|October) \\d+, \\d{4}"))
 Speechdataframe<-cbind(as.data.frame(first_html),Dateinfo)
-colnames(Speechdataframe)<-c("first_URL","Date")
+Speechdataframe[,2]=str_replace_all(Speechdataframe[,2],"(September|October) \\d+, ","")
+colnames(Speechdataframe)<-c("first_URL","Year")
+
+###Write a function about how to extract URL of a year given year as an input
+select_url<-function(year){
+  return(Speechdataframe[Speechdataframe[,2]==year,1])
+}
+
 
 ##B
-speech_data<-htmlParse(Speechdataframe[1,1])
+speech_data<-htmlParse(select_url(2012))
 ## By inspecting the Xpath Code of the element in Chrome.
 text_data<-xpathSApply(speech_data,"//p/text()",xmlValue)
 ##Good Look
 cat(paste(text_data,collapse="\n\n"))
 
+##B
 text_data<-as.list(text_data)
 # z <- lapply(text_data
 #        ,function(x){y <-as.character(str_match(string=x, pattern="^[A-Z]+:"))
 #                     return(y)})
 # z
 speakernames <- as.list(str_replace(as.list(str_match(text_data,"^[A-Z]+:")),":",replacement=""))
-# speakernames<-as.list(str_match(text_data,"^[A-Z]+:"))
+##Set up
 finalframe<-cbind(speakernames,text_data)
 
 speakerposition<-1
@@ -62,7 +70,40 @@ finalframe<-finalframe[is.na(finalframe[,1])==FALSE,]
 ##This step strip out the useless Laughter Applause and crosstalk tags from the speech.
 ## Also strip out the speaker name at the very top of each paragraph
 finalframe[,2]<-str_replace_all(finalframe[,2],"\\(LAUGHTER\\)|\\(APPLAUSE\\)|\\(CROSSTALK\\)|^[A-Z]+:","")
-
+finalframe[,2]<-str_replace_all(finalframe[,2],"^ ","")
 ##Split into sentences and words
-a<-str_split(finalframe[,2],pattern = "\\. |? ")
+sentencesplit<-str_split(finalframe[,2],pattern = "\\. |\\? |\\, |\\ -- |\\.\\.\\. ")
+# b<-str_split(a,pattern=" ")
+wordsplit<-lapply(sentencesplit,function(x){return(str_split(x,pattern="\\ "))})
+##Count total number of words and characters
+finalframe<-cbind(finalframe,wordsplit)
 
+###Write a function that will return the data required for a speech.
+Candidate_stat<-function(finalframe){
+  ##Store speaker names to a vector
+  speaker_unique<-unique(finalframe[finalframe[,1]!="SPEAKERS",1])
+  ##Create an empty data frame to store number of words, average length, etc.
+  candidate_data<-data.frame(matrix(numeric(0),ncol=15,nrow=3),stringsAsFactors=FALSE)
+  colnames(candidate_data)<-c("wordcount","charachtercount","averagelength",
+                              "I","we","American","democracy","republic",
+                              "Democrat","Republican","freedom",
+                              "war","God","God Bless","Jesus")
+  rownames(candidate_data)<-speaker_unique
+  ##Now all splitting in word is in the third column of the finalframe
+  for (i in 1:length(speaker_unique)){
+    name=speaker_unique[[i]]
+    word_candidate=unlist(finalframe[finalframe[,1]==name,3])
+    candidate_data$wordcount[i]<-length(word_candidate)
+    candidate_data$charachtercount[i]<-sum(nchar(word_candidate))
+    candidate_data$averagelength[i]=candidate_data$charachtercount[i]/candidate_data$wordcount[i]
+    candidate_data$American[i]<-sum(str_count(word_candidate,"American?"))
+    candidate_data$I[i]<-sum(str_count(word_candidate,"I\\b"))
+    candidate_data$we[i]<-sum(str_count(word_candidate,"We\\b|we\\b"))
+    candidate_data$democracy[i]<-sum(str_count(word_candidate,"democracy\\b|democratic\\b"))
+    candidate_data$republic[i]<-sum(str_count(word_candidate,"republic\\b|Republic\\b"))
+    candidate_data$Democrat[i]<-sum(str_count(word_candidate,"Democrats?[ic]?\\b"))
+    candidate_data$Republican[i]<-sum(str_count(word_candidate,"Republicans?"))
+    candidate_data$freedom[i]<-sum(str_count(word_candidate,"free[dom]?"))
+  }
+  return(candidate_data)
+}
