b670bfd330c1de9e7ca03bdde8b068499a19bedc
pttzty <tyzhang@berkeley.edu>
2015-10-20 23:28:48 -0700

    tran

--- ps6/PS6.R
+++ ps6/PS6.R
@@ -10,7 +10,7 @@ for (i in 1987:1988){
   bzname=paste(toString(i),".csv.bz2",sep="")
   con1<-file(bzname,open="r")
   datacsv<-read.csv(con1,header=TRUE)
-  dbWriteTable(conn=db,value=datacsv,name="year",row.names=FALSE,append=TRUE)
+  dbWriteTable(conn=db,value=datacsv,name="year",row.names=FALSE)
   # auth<-dbSendQuery(db,"select * from year where Month=10 or 11 limit 100")
   close(con1)
   rm(datacsv)
