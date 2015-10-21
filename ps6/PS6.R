6f7237cd8e3512f74930d66d66eb288296b0b408
pttzty <tyzhang@berkeley.edu>
2015-10-20 22:47:43 -0700

    For transfer

---
+++ ps6/PS6.R
@@ -0,0 +1,18 @@
+# install.packages("RSQLite")
+library(RSQLite)
+library(stringr)
+filename<-"airline_data.db"
+drv<-dbDriver("SQLite")
+db<-dbConnect(drv,dbname=filename)
+
+system.time(
+for (i in 1987:1988){
+  bzname=paste(toString(i),".csv.bz2",sep="")
+  con1<-file(bzname,open="r")
+  datacsv<-read.csv(con1,header=TRUE)
+  dbWriteTable(conn=db,value=datacsv,name="year",row.names=FALSE,append=TRUE)
+  # auth<-dbSendQuery(db,"select * from year where Month=10 or 11 limit 100")
+  close(con1)
+  rm(datacsv)
+}
+)
